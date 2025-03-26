4b84bcb637a167c38f94b873a56d6a57c2873b41
lakshanFernando2003 <lakshanchanaka34@gmail.com>
2025-03-26 19:48:49 +0530

    completed Movie.java file

---
+++ src/com/rootcode/movierating/model/Movie.java
@@ -0,0 +1,74 @@
+package com.rootcode.movierating.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String genre; // fixed types only ~ Horror, Action, Romantic, Comedy, Science Fiction
+    private List <Rating> ratings;
+
+    public Movie(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+        this.ratings = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void addRating(Rating rating) {
+        this.ratings.add(rating);
+    }
+
+    // Method to get the average rating of the movie
+    public double getAvarageRating() {
+        if (ratings.isEmpty()) return 0.0;
+
+        int total = 0;
+        for  (Rating rating : ratings) {
+            total += rating.getStars();
+        }
+        return total / (double) ratings.size();
+    }
+
+     // Method to get the top rated movie
+    public boolean isTopRatedEligible(){
+        return ratings.size() >= 5; // A movie must have at least 5 ratings to be eligible for top rated
+    }
+
+    public List<Rating> getRatingsByStar (int star){
+        List<Rating> filteredRating = new ArrayList<>();
+        for (Rating rating : ratings) {
+            if (rating.getStars() == star) {
+                filteredRating.add(rating);
+            }
+        }
+        return filteredRating;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + genre + ") - Avg Rating: " + String.format("%.2f", getAvarageRating());
+    }
+
+}
+
+
